4e51ba4ebe0493db4d1b54381790d9374ae51e23
kreslavskiy <mishak1000236@gmail.com>
2023-03-19 13:40:58 +0200

    Added function for input validation

--- implementation.go
+++ implementation.go
@@ -3,6 +3,7 @@ package lab2
 import (
 	"fmt"
 	"math"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -15,7 +16,29 @@ func splice(array []string, startIndex int, count int) []string {
 	return array
 }
 
+func validate(input string) (bool, error) {
+	spaces := strings.Contains(input, " ")
+	numbers := regexp.MustCompile(`\d`).MatchString(input)
+	if spaces && numbers {
+		splited := strings.Split(input, " ")
+		length := len(splited)
+
+		penultimateIsNumber := regexp.MustCompile(`\d`).MatchString(splited[length - 2])
+		lastIsNumber := regexp.MustCompile(`\d`).MatchString(splited[length - 1])
+
+		if penultimateIsNumber && lastIsNumber {
+			return true, nil
+		}
+	}
+	return false, fmt.Errorf("Expression contains invalid characters")
+}
+
 func CalculatePolishNotation(input string) (string, error) {
+	_, err := validate(input)
+	if err != nil {
+		return "Consider that expression must contain only numbers and operands separated by empty string", fmt.Errorf("Expression contains invalid characters")
+	}
+
 	inputNormalized := strings.Split(input, " ")
 
 	for {
